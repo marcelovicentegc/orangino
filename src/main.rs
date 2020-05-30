b8c3f9f30dfa05b3564ffc209f5b175e0f5f6b53
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-05-30 20:29:47 -0300

    chore: add stronger typing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+extern crate chrono;
 extern crate dotenv;
+use chrono::NaiveDateTime;
 use console::{style, Emoji};
 use dotenv::dotenv;
 use pyo3::prelude::*;
 use reqwest::header::{HeaderMap, HeaderValue};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::env;
 
@@ -14,24 +15,60 @@ struct PunchResp {
     allowAll: bool,
 }
 
-static ASTRONAUT: Emoji<'_, '_> = Emoji("👩‍🚀  ", "");
-static PUNCH: Emoji<'_, '_> = Emoji("👊  ", "");
-static LINK: Emoji<'_, '_> = Emoji("🔗  ", "");
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: u32,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct Employee {
+    id: u32,
+    nome: String,
+    usuario: User,
+    selectedDataInicio: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct CheckUserResp {
+    status: String,
+    funcionario: Employee,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize)]
+struct SyncResp {
+    tipoRetornoRegistroApontamentoEnum: String,
+    mensagem: String,
+    statusPonto: i32,
+    sucesso: bool,
+    excluirPonto: bool,
+}
+
+static ASTRONAUT: Emoji<'_, '_> = Emoji("👩‍🚀 ", "");
+static PUNCH: Emoji<'_, '_> = Emoji("👊 ", "");
+static LINK: Emoji<'_, '_> = Emoji("🔗 ", "");
 static TAKING_NOTE: Emoji<'_, '_> = Emoji("📝 ", "");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
+    #[allow(non_snake_case)]
+    let EMPLOYER_CODE = env::var("EMPLOYER_CODE").unwrap();
+    #[allow(non_snake_case)]
+    let PIN = env::var("PIN").unwrap();
+
     let check_user_status_url = format!(
         "https://app.tangerino.com.br/Tangerino/ws/fingerprintWS/funcionario/empregador/{}/pin/{}",
-        &env::var("EMPLOYER_CODE").unwrap(),
-        &env::var("PIN").unwrap()
+        EMPLOYER_CODE, PIN
     );
     let punch_url = format!(
         "https://app.tangerino.com.br/Tangerino/ws/autorizaDipositivoWS/verifica/web/empregador/{}/pin/{}", 
-        &env::var("EMPLOYER_CODE").unwrap(),
-        &env::var("PIN").unwrap()
+        EMPLOYER_CODE,
+        PIN
     );
     let sync_punch_url =
         "https://app.tangerino.com.br/Tangerino/ws/pontoWS/ponto/sincronizacaoPontos/1.2";
@@ -43,7 +80,120 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let check_user_resp = reqwest::get(&check_user_status_url).await?;
 
     if check_user_resp.status().is_success() {
-        println!("Success getting user status");
+        let parsed_check_user_resp: CheckUserResp =
+            serde_json::from_str(&check_user_resp.text().await?)?;
+
+        // This means there is a valid user associated with the
+        // provided employer and pin codes
+        if parsed_check_user_resp.status == "SUCCESS" {
+            let client = reqwest::Client::new();
+            let mut payload = HashMap::new();
+            payload.insert("deviceId", "null");
+            println!("Hi there, {}!", parsed_check_user_resp.funcionario.nome);
+            println!("{} {}Punching card...", style("[2/4]").bold().dim(), PUNCH);
+
+            let punch_resp = client.post(&punch_url).json(&payload).send().await?;
+
+            if punch_resp.status().is_success() {
+                println!("Success on punching the card");
+
+                let parsed_punch_resp: PunchResp = serde_json::from_str(&punch_resp.text().await?)?;
+
+                if parsed_punch_resp.allowAll == true {
+                    let mut headers = HeaderMap::new();
+                    let employee_id = &parsed_check_user_resp.funcionario.id.to_string();
+                    let raw_dt = &parsed_check_user_resp
+                        .funcionario
+                        .selectedDataInicio
+                        .replace("T", " ");
+                    println!("raw_dt: {}", raw_dt);
+                    let dt = NaiveDateTime::parse_from_str(raw_dt, "%Y-%m-%d %H:%M:%S")?;
+                    println!("dt: {}", dt);
+                    let punch_in_date = dt.format("%d/%m/%y %H:%M:%S").to_string();
+                    let punch_payload = format!("{{\"horaInicio\": \"{}\",\"deviceId\": null,\"online\": \"true\",\"codigoEmpregador\": \"{}\",\"pin\": \"{}\",\"horaFim\": \"\",\"tipo\": \"WEB\",\"foto\": \"\",\"intervalo\": \"\",\"validFingerprint\": false,\"versao\": \"registra-ponto-fingerprint\",\"plataforma\": \"WEB\",\"funcionarioid\": {},\"idAtividade\": 1,\"latitude\": null,\"longitude\": null}}",
+                        &punch_in_date, &EMPLOYER_CODE, &PIN, employee_id
+                    );
+
+                    headers.insert(
+                        reqwest::header::CONTENT_TYPE,
+                        HeaderValue::from_str("application/json").unwrap(),
+                    );
+                    headers.insert("empregador", HeaderValue::from_str(&EMPLOYER_CODE).unwrap());
+                    headers.insert(
+                        "funcionarioid",
+                        HeaderValue::from_str(&employee_id).unwrap(),
+                    );
+                    headers.insert("username", HeaderValue::from_str(&employee_id).unwrap());
+                    headers.insert(
+                        "authorization",
+                        HeaderValue::from_str(
+                            &env::var("TANGERINO_BASIC_TOKEN").unwrap().to_string(),
+                        )
+                        .unwrap(),
+                    );
+
+                    assert!(headers.contains_key("empregador"));
+                    assert!(headers.contains_key("funcionarioid"));
+                    assert!(headers.contains_key("username"));
+                    assert!(headers.contains_key("authorization"));
+
+                    println!(
+                        "{} {}Syncing punch record...",
+                        style("[3/4]").bold().dim(),
+                        LINK
+                    );
+
+                    let sync_resp = client
+                        .post(sync_punch_url)
+                        .headers(headers)
+                        .body(punch_payload)
+                        .send()
+                        .await?;
+
+                    if sync_resp.status().is_success() {
+                        let parsed_sync_resp: SyncResp =
+                            serde_json::from_str(&sync_resp.text().await?)?;
+
+                        if parsed_sync_resp.sucesso == false {
+                            println!(
+                                "{}",
+                                parsed_sync_resp
+                                    .tipoRetornoRegistroApontamentoEnum
+                                    .replace("_", " ")
+                            );
+                            println!("{}", parsed_sync_resp.mensagem);
+                        } else {
+                            let gil = Python::acquire_gil();
+                            let py = gil.python();
+                            println!(
+                                "{} {}Publishing on Slack...",
+                                style("[4/4]").bold().dim(),
+                                TAKING_NOTE
+                            );
+                            publish_to_slack(py).map_err(|e| {
+                                // We can't display Python exceptions via std::fmt::Display,
+                                // so print the error here manually.
+                                e.print_and_set_sys_last_vars(py);
+                            });
+                        }
+                    } else if sync_resp.status().is_server_error() {
+                        println!("Failed to syncrhonize the punch record");
+                    } else {
+                        println!(
+                        "Something else happened while trying to synchronize the punch record. Status: {:?}",
+                        sync_resp.status()
+                    );
+                    }
+                }
+            } else if punch_resp.status().is_server_error() {
+                println!("Failed to punch the card");
+            } else {
+                println!(
+                    "Something else happened while trying to punch the card. Status: {:?}",
+                    punch_resp.status()
+                );
+            }
+        }
     } else if check_user_resp.status().is_server_error() {
         println!("Failed to get user status");
     } else {
@@ -51,111 +201,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "Something else happened while trying to get user status. Status: {:?}",
             check_user_resp.status()
         );
-    }
-
-    let parsed_check_user_resp: Value = serde_json::from_str(&check_user_resp.text().await?)?;
-
-    // This means there is a valid
-    // user associated with the
-    // provided employer and pin
-    // codes
-    if parsed_check_user_resp["status"] == "SUCCESS" {
-        let client = reqwest::Client::new();
-        let mut map = HashMap::new();
-        map.insert("deviceId", "null");
-
-        println!("{} {}Punching card...", style("[2/4]").bold().dim(), PUNCH);
-        let punch_resp = client.post(&punch_url).json(&map).send().await?;
-        if punch_resp.status().is_success() {
-            println!("Success on punching the card");
-
-            let parsed_punch_resp: PunchResp = serde_json::from_str(&punch_resp.text().await?)?;
-
-            if parsed_punch_resp.allowAll == true {
-                let mut headers = HeaderMap::new();
-                let punch_payload = json!({
-                    "horaInicio": parsed_check_user_resp["selectedDataInicio"].to_string(),
-                    "deviceId": parsed_check_user_resp["deviceId"].to_string(),
-                    "online": true,
-                    "codigoEmpregador": env::var("EMPLOYER_CODE").unwrap(),
-                    "pin": env::var("PIN").unwrap(),
-                    "horaFim": "",
-                    "tipo": "WEB",
-                    "foto": "",
-                    "intervalo": "",
-                    "validFingerprint": false,
-                    "versao": "registra-ponto-fingerprint",
-                    "plataforma": "WEB",
-                    "funcionarioid": parsed_check_user_resp["funcionario"]["id"]
-                        .to_string()
-                        .parse::<i32>()
-                        .unwrap(),
-                    "idAtividade": 1,
-                    "latitude": "",
-                    "longitude": "",
-                });
-
-                let employer =
-                    HeaderValue::from_str(&parsed_check_user_resp["empregador"].to_string());
-                let employee =
-                    HeaderValue::from_str(&parsed_check_user_resp["funcionario"].to_string());
-                let username =
-                    HeaderValue::from_str(&parsed_check_user_resp["username"].to_string());
-                headers.insert("empregador", employer.unwrap());
-                headers.insert("funcionario", employee.unwrap());
-                headers.insert("username", username.unwrap());
-                headers.insert(
-                    "authorization",
-                    HeaderValue::from_str(&env::var("TANGERINO_BASIC_TOKEN").unwrap().to_string())
-                        .unwrap(),
-                );
-                assert!(headers.contains_key("empregador"));
-                assert!(headers.contains_key("funcionario"));
-                assert!(headers.contains_key("username"));
-                assert!(headers.contains_key("authorization"));
-                println!(
-                    "{} {}Syncing punch record...",
-                    style("[3/4]").bold().dim(),
-                    LINK
-                );
-                let sync_resp = client
-                    .post(sync_punch_url)
-                    .json(&punch_payload)
-                    .headers(headers)
-                    .send()
-                    .await?;
-
-                if sync_resp.status().is_success() {
-                    println!("Success on synchronizing the punch record");
-                    let gil = Python::acquire_gil();
-                    let py = gil.python();
-                    println!(
-                        "{} {}Publishing on Slack...",
-                        style("[4/4]").bold().dim(),
-                        TAKING_NOTE
-                    );
-                    publish_to_slack(py).map_err(|e| {
-                        // We can't display Python exceptions via std::fmt::Display,
-                        // so print the error here manually.
-                        e.print_and_set_sys_last_vars(py);
-                    });
-                } else if sync_resp.status().is_server_error() {
-                    println!("Failed to syncrhonize the punch record");
-                } else {
-                    println!(
-                        "Something else happened while trying to synchronize the punch record. Status: {:?}",
-                        sync_resp.status()
-                    );
-                }
-            }
-        } else if punch_resp.status().is_server_error() {
-            println!("Failed to punch the card");
-        } else {
-            println!(
-                "Something else happened while trying to punch the card. Status: {:?}",
-                punch_resp.status()
-            );
-        }
     }
     Ok(())
 }
